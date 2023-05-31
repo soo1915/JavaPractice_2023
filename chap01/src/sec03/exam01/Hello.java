22ada19d7f74bcd6d6d848c7a3e21db78f723820
1 <1@admin>
2023-05-31 16:42:03 +0900

    chapter 01

---
+++ chap01/src/sec03/exam01/Hello.java
@@ -0,0 +1,11 @@
+package sec03.exam01;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.print("Hello, Java");
+		System.out.println("¾È³ç");
+		System.out.println();
+	}
+
+}
