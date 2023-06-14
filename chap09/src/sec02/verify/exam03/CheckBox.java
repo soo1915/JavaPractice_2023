38af4809301802936c1390b1168eaf374c02c82c
soo1915 <soo1915@naver.com>
2023-06-14 16:40:18 +0900

    anonymous final

---
+++ chap09/src/sec02/verify/exam03/CheckBox.java
@@ -0,0 +1,18 @@
+package sec02.verify.exam03;
+
+public class CheckBox {
+
+	OnSelectListener listener;
+	
+	void setOnSelectListener(OnSelectListener listener) {
+		this.listener = listener;
+	}
+	
+	void select() {
+		listener.onSelect();
+	}
+	
+	static interface OnSelectListener {
+		void onSelect();
+	}
+}
