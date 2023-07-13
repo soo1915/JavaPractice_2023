644bd6b69a3b011f6823b1c6dbd4c23d1b9a541d
soo1915 <soo1915@naver.com>
2023-07-13 17:12:28 +0900

    servlet & jstl

--- oracle0703/src/main/java/board/oracle/servlet/ListServ.java
+++ oracle0703/src/main/java/board/oracle/servlet/ListServ.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -15,7 +14,7 @@ import board.oracle.BoardDto;
 /**
  * Servlet implementation class ListServ
  */
-@WebServlet("/listServ")
+//@WebServlet("/listServ")
 public class ListServ extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
