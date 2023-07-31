83eef440c12c8dee1041f272612ef2e459463c64
soo1915 <soo1915@naver.com>
2023-07-31 17:47:27 +0900

    spring chapter 4 example

---
+++ sp5-chap04Practice/src/main/java/spring/MemberPrinter.java
@@ -0,0 +1,43 @@
+package spring;
+
+import java.time.format.DateTimeFormatter;
+
+public class MemberPrinter {
+	private DateTimeFormatter dateTimeFormatter;
+	
+//	public MemberPrinter() {
+//		dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy년 MM월 dd일");
+//	}
+	
+	public void print(Member member) {
+		if (dateTimeFormatter == null) {
+			System.out.printf(
+					"회원 정보: 아이디=%d, 이메일=%s, 이름=%s, 등록일=%tF\n", 
+					member.getId(), member.getEmail(),
+					member.getName(), member.getRegisterDateTime());
+		} else {
+			System.out.printf(
+					"회원 정보: 아이디=%d, 이메일=%s, 이름=%s, 등록일=%s\n", 
+					member.getId(), member.getEmail(),
+					member.getName(), 
+					dateTimeFormatter.format(member.getRegisterDateTime()));
+		}
+	}
+	
+	public void setDateFormatter(DateTimeFormatter dateTimeFormatter) {
+		this.dateTimeFormatter = dateTimeFormatter;
+	}
+
+//	public void setDateFormatter(Optional<DateTimeFormatter> formatterOpt) {
+//		if (formatterOpt.isPresent()) {
+//			this.dateTimeFormatter = formatterOpt.get();
+//		} else {
+//			this.dateTimeFormatter = null;
+//		}
+//	}
+	
+//	public void setDateFormatter(DateTimeFormatter dateTimeFormatter) {
+//		this.dateTimeFormatter = dateTimeFormatter;
+//	}
+	
+}
