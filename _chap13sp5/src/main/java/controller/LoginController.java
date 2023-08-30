e1283796dc1b82992df103681ce9ae306df35e5d
soo1915 <soo1915@naver.com>
2023-08-30 16:32:29 +0900

    spring chap 13 finished

--- _chap13sp5/src/main/java/controller/LoginController.java
+++ _chap13sp5/src/main/java/controller/LoginController.java
@@ -1,9 +1,12 @@
 package controller;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,13 +28,17 @@ public class LoginController {
 	}
 	
 	@GetMapping
-	public String Form(LoginCommand loginCommand) { // 매개변수를 지우고 아래 문장을 작성하는 것과 동일
+	public String Form(LoginCommand loginCommand, @CookieValue(value = "REMEMBER", required = false) Cookie rCookie) { // 매개변수를 지우고 아래 문장을 작성하는 것과 동일
 		//model.addAttribute("loginCommand", new LoginCommand());
+		if (rCookie != null) {
+			loginCommand.setEmail(rCookie.getValue());
+			loginCommand.setRememberEmail(true);
+		}
 		return "login/loginForm";
 	}
 	
 	@PostMapping
-	public String submit(LoginCommand loginCommand, Errors errors, HttpSession session) {
+	public String submit(LoginCommand loginCommand, Errors errors, HttpSession session, HttpServletResponse response) {
 		new LoginCommandValidator().validate(loginCommand, errors);
 		if (errors.hasErrors()) {
 			return "login/loginForm";
@@ -40,9 +47,18 @@ public class LoginController {
 			AuthInfo authInfo = authService.authenticate(loginCommand.getEmail(), loginCommand.getPassword());
 			session.setAttribute("authInfo", authInfo);
 			session.setAttribute("test", authInfo);
-			// 세션에 autoInfo 저장해야 함
-			AuthInfo authInfo1 = (AuthInfo) session.getAttribute("authInfo");
-			log.info("======================"+authInfo1);
+			// 세션에 authInfo 저장해야 함
+			
+			Cookie rememberCookie = new Cookie("REMEMBER", loginCommand.getEmail());
+			rememberCookie.setPath("/"); // 쿠키 저장 경로(기억 못하니까 그냥 root로 저장)
+			if (loginCommand.isRememberEmail()) {// 이메일 기억하기를 선택했는지 여부
+				rememberCookie.setMaxAge(60 * 60 * 24 * 30); //30일 동안 유지되는 쿠키 생성
+			} else {
+				rememberCookie.setMaxAge(0); //바로 삭제되는 쿠키 생성
+			}
+			response.addCookie(rememberCookie);
+//			AuthInfo authInfo1 = (AuthInfo) session.getAttribute("authInfo");
+//			log.info("======================"+authInfo1);
 			return "login/loginSuccess";
 		} catch(WrongIdPasswordException e) {
 			errors.reject("idPasswordNotMatching");
